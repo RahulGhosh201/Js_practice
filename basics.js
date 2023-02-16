80d56efaf311eadc438ab0a8d3aeb18c90d23ec8
RahulGhosh201 <99985624+RahulGhosh201@users.noreply.github.com>
2023-02-17 03:04:06 +0530

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,18 @@
+console.log('rahul ghosh')
+
+//variables
+name='Peter pan'
+console.log(name)
+
+
+//operators
+a=2
+b=5
+c=a*b
+console.log(c)
+
+//user input
+//prompt() is the functtion which gets gets input from user
+
+fruit=prompt('which fruit do you want?')
+console.log(fruit)
