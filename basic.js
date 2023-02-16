f7bbc82a1ea80a14cc8fbe2706e832ff9c50e850
RahulGhosh201 <99985624+RahulGhosh201@users.noreply.github.com>
2023-02-17 03:08:41 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,71 @@
+// function greeting(name)
+// {
+//     let greet=`hi ${name},Nice to meet you`
+//     console.log(greet)
+// }
+
+// greeting('Rahul')
+// function sum(a,b)
+// {
+//     return a+b
+// }
+// console.log(sum(1,2))
+// num=sum(5,7)
+// console.log(num)
+
+// function calculateTotal(food,tip)
+// {
+//     tipPercentage=tip/100
+//     tipAmount=food*tipPercentage
+//     total=sum(food,tipAmount)
+//     return total
+// }
+// x=calculateTotal(1000,5)
+// console.log(`Total amount: ${x}`)
+
+
+// const sumArray=(a,b)=>a+b
+// console.log(sumArray(5,8))
+
+// let groceries=['apple','pear','banana']
+
+// groceries.push('guava')
+// console.log(groceries)
+// groceries.pop('guava')
+// console.log(groceries)
+// console.log(groceries.slice(1,2))
+
+
+// const person={
+// 	name:'Leonardo',
+// 	shirt:'White'
+// }
+// console.log(person.name)  
+//  console.log(person['name'])
+// console.log(person.shirt)  
+//  console.log(person['shirt'])
+//  person.phone='123456'
+//  console.log(person.phone)
+//  console.log(person)
+
+// const person2={
+//     name:'rahul',
+//     shirt:'black'
+// }
+
+// console.log(person2)
+
+const introducer=(name,shirt)=>
+{
+    const person={
+        name:name,
+        shirt:shirt
+    }
+    const intro=`Hi my name is ${person.name} & my shirt is ${person.nameshirt}`
+    return intro
+}
+
+//console.log(introducer('rahul','black'))
+
+
+
