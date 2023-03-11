bbef3b76a760e0213ad934ad8fb7eee11158eb49
RahulGhosh201 <99985624+RahulGhosh201@users.noreply.github.com>
2023-03-11 13:49:17 +0530

    Delete dom.js

--- dom.js
+++
@@ -1,7 +0,0 @@
-const actors=[
-	{name: 'johny' , networth:10000},
-	{name: 'amber' , networth:10},
-	{name: 'leonardo' , networth:100000000}
-]
-let ressult=actors.filter(actor=>actor.networth>10)
-dom.innerHTML=`<h1>${ressult[0].name}</h1>`
