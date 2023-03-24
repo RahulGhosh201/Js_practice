55a56b7c723ba258de37b44c44d8a6d723dcfaab
RahulGhosh201 <99985624+RahulGhosh201@users.noreply.github.com>
2023-03-24 20:26:45 +0530

    Delete dom directory

--- dom/script.js
+++
@@ -1,8 +0,0 @@
-console.log('Hello')
-let message='GOODBYE to you all'
-let title=document.getElementById('title')
-console.log('before:',title.innerText)
-title.innerText='GoodBye'
-console.log('after:',title.innerText)
-title.innerHTML=`${message}`
-title.style.backgroundColor = 'red'
